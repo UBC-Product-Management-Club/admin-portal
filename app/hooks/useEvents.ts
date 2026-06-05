4813325456f565bb2a448ea5b0d80f1e0c80de78
Evan Peng <evanpeng2000@gmail.com>
2026-06-04 17:15:20 -0700

    Implemented parsing in EventService

--- app/hooks/useEvents.ts
+++ app/hooks/useEvents.ts
@@ -1,25 +1,23 @@
-import { BasicEventsSchema, EventSchema } from "@/lib/types/Event";
-import { EventAttendeesResponseSchema } from "@/lib/types/User";
 import { EventService } from "@/services/EventService";
 import { useCallback, useMemo } from "react";
 
 export function useEvents() {
     const eventService = useMemo(() => new EventService, [])
 
-    const getBasicEventInfo = useCallback(async () => {
-        const data = await eventService.getBasicEventInfo()
-        return BasicEventsSchema.parse(data)
-    }, [eventService])
+    const getBasicEventInfo = useCallback(
+        () => eventService.getBasicEventInfo(),
+        [eventService]
+    )
 
-    const getEvent = useCallback(async (event_id: string) => {
-        const data = await eventService.getEvent(event_id)
-        return EventSchema.parse(data)
-    }, [eventService])
+    const getEvent = useCallback(
+        (event_id: string) => eventService.getEvent(event_id),
+        [eventService]
+    )
 
-    const getEventAttendees = useCallback(async (event_id: string) => {
-        const data = await eventService.getEventAttendees(event_id)
-        return EventAttendeesResponseSchema.parse(data)
-    }, [eventService])
+    const getEventAttendees = useCallback(
+        (event_id: string) => eventService.getEventAttendees(event_id),
+        [eventService]
+    )
 
     return { getBasicEventInfo, getEvent, getEventAttendees }
-} 
+}
