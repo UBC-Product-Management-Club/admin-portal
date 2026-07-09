538320fb623a725d056aa200923ea9eaec208e49
Kashish Garg <kashishgarg247@gmail.com>
2026-07-09 19:06:36 -0400

    Fix missing IconSettings import in admin sidebar

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
-import { IconChartBar, IconDashboard, IconFolder, IconListDetails } from '@tabler/icons-react';
+import {
+  IconChartBar,
+  IconDashboard,
+  IconFolder,
+  IconListDetails,
+  IconSettings,
+} from '@tabler/icons-react';
 
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
